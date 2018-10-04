4ce68784339cf3592dc4606aa6d6897c3bd55de4
Karan Dhanoya <kdhanoya94@gmail.com>
2018-10-04 14:22:44 +0100

    connecting to database with postgresql

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const cons = require('consolidate')
 const dust = require('dustjs-helpers')
-const pg = require('pg')
+const cons = require('consolidate')
+const knex = require('knex')
+
+
+const postgres =  knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'recipebookdb'
+    }
+  });
 
 const app = express();
-
-const conString = "postgres://:@localhost/recipebookdb"
 
 app.engine('dust', cons.dust)
 app.set('view engine', 'dust')
 app.set('views', __dirname + '/views')
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-    console.log('TEST')
-    res.render('index')
+    postgres.select().from('recipes')
+    .then((recipe) => {
+        console.log(recipe)
+        res.render('index', {
+            recipe: recipe           
+        });
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
 })
 
-app.listen(3000, function(){
+app.listen(3050, function(){
     console.log('Server is on!')
 })
 
